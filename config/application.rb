bf3cfc6c68c13856341b02e0fac3ec78a199e8e0
Ryan Morrison~Westphal <ryan@trippingthebits.com>
2016-07-15 23:38:36 -0400

    load specific classifier classes

--- config/application.rb
+++ config/application.rb
@@ -29,6 +29,9 @@ module Classifurlr
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
 
+    # auto-load classifier classes
+    config.autoload_paths += %W(#{config.root}/app/models/classifiers)
+
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
   end
